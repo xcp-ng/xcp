3b00ed35a92684cd5a593cb6d7dddbf144f5f4da
Samuel Verschelde <stormi@laposte.net>
2019-02-21 17:45:25 +0100

    Update sync_repo_from_koji.py
    - update current list of tags for XCP-ng 7.6
    - do not update the base repository for stable releases by default
    - add a --modify-stable-base switch to force updating it
    - use rsync with specific options so that the timestamps for unmodified
      RPMs are not touched
    - add missing --debuginfo switch to koji download-build
    - better log messages
    - for all tags but the stable base, include all RPM versions, not just
      the latest

--- scripts/koji/sync_repo_from_koji.py
+++ scripts/koji/sync_repo_from_koji.py
@@ -9,15 +9,21 @@ import shutil
 import tempfile
 
 VERSIONS = [
-    '7.6'
+    '7.6',
+]
+
+DEV_VERSIONS = [
 ]
 
 TAGS = [
-    '7.6',
-    '7.6-updates',
-    '7.6-updates_testing',
-    '7.6-extras',
-    '7.6-extras_testing'
+    'v7.6-base',
+    'v7.6-updates',
+    'v7.6-testing',
+]
+
+# tags in which we only keep the latest build for each package
+RELEASE_TAGS = [
+    'v7.6-base',
 ]
 
 KOJI_ROOT_DIR = '/mnt/koji'
@@ -25,15 +31,12 @@ KOJI_ROOT_DIR = '/mnt/koji'
 KEY_ID = "3fd3ac9e"
 
 def version_from_tag(tag):
-    matches = re.match('\d+\.\d+', tag)
-    return matches.group(0)
+    matches = re.match(r'v(\d+\.\d+)', tag)
+    return matches.group(1)
 
 def repo_name_from_tag(tag):
     version = version_from_tag(tag)
-    if tag == version:
-        return 'base'
-    else:
-        return tag[len(version)+1:]
+    return tag[len("v%s-" % version):]
 
 def sign_rpm(rpm):
     # create temporary work directory
@@ -44,7 +47,7 @@ def sign_rpm(rpm):
         os.chdir(tmpdir)
 
         # download from koji
-        subprocess.check_call(['koji', 'download-build', '--rpm', rpm])
+        subprocess.check_call(['koji', 'download-build', '--debuginfo', '--rpm', rpm])
 
         # sign: requires a sign-rpm executable or alias in the PATH
         subprocess.check_call(['sign-rpm', rpm])
@@ -64,33 +67,51 @@ def write_repo(tag, dest_dir):
     version = version_from_tag(tag)
     major = version.split('.')[0]
     repo_name = repo_name_from_tag(tag)
-    path_to_repo = os.path.join(dest_dir, 'repo', major, version, repo_name)
+    path_to_repo = os.path.join(dest_dir, major, version, repo_name)
 
     # Temporary hack to write 7.6 packages in a different directory,
     # because the main source for 7.6 packages is not koji yet and we don't
     # want to override the repo.
     path_to_repo = path_to_repo.replace('/7.6/', '/7.6-test/')
 
+    path_to_tmp_repo = path_to_repo + '-tmp'
+
     print(path_to_repo)
 
-    # remove repo if exists
-    if os.path.isdir(path_to_repo):
-        shutil.rmtree(path_to_repo)
+    # remove temporary repo if exists
+    if os.path.isdir(path_to_tmp_repo):
+        shutil.rmtree(path_to_tmp_repo)
 
     # create empty structure
+    print("\n-- Copy the RPMs from %s to %s" % (KOJI_ROOT_DIR, path_to_tmp_repo))
     for d in ['x86_64/Packages', 'Source/SPackages']:
-        os.makedirs(os.path.join(path_to_repo, d))
+        os.makedirs(os.path.join(path_to_tmp_repo, d))
 
     # copy RPMs from koji
     for f in glob.glob('%s/repos-dist/%s/latest/x86_64/Packages/*/*.rpm' % (KOJI_ROOT_DIR, tag)):
-        shutil.copy(f, os.path.join(path_to_repo, 'x86_64', 'Packages'))
+        shutil.copy(f, os.path.join(path_to_tmp_repo, 'x86_64', 'Packages'))
 
     # and source RPMs
     for f in glob.glob('%s/repos-dist/%s/latest/src/Packages/*/*.rpm' % (KOJI_ROOT_DIR, tag)):
-        shutil.copy(f, os.path.join(path_to_repo, 'Source', 'SPackages'))
+        shutil.copy(f, os.path.join(path_to_tmp_repo, 'Source', 'SPackages'))
+
+    # Synchronize to our final repository:
+    # - add new RPMs
+    # - remove RPMs that are not present anymore (for tags in RELEASE_TAGS)
+    # - do NOT change the creation nor modification stamps for existing RPMs that have not been modified
+    #   (and usually there's no reason why they would have been modified without changing names)
+    #   => using -c and omitting -t
+    print("\n-- Syncing to final repository %s" % path_to_repo)
+    if not os.path.exists(path_to_repo):
+        os.makedirs(path_to_repo)
+    subprocess.check_call(['rsync', '-crlpvP', '--exclude=repodata/', '--delete-after',
+                           path_to_tmp_repo + '/', path_to_repo])
+    print()
+    shutil.rmtree(path_to_tmp_repo)
 
     # generate repodata and sign
     for path in [os.path.join(path_to_repo, 'x86_64'), os.path.join(path_to_repo, 'Source')]:
+        print("\n-- Generate repodata for %s" % path)
         subprocess.check_call(['createrepo_c', path])
         subprocess.check_call(['sign-file', os.path.join(path, 'repodata', 'repomd.xml')])
 
@@ -102,11 +123,15 @@ def sign_unsigned_rpms(tag):
     rpms = set(output.strip().splitlines())
 
     # only signed RPMs
-    # koji list-tagged 7.6 --sigs | grep "^3fd3ac9e" | cut -c 10-
+    # koji list-tagged v7.6-base --sigs | grep "^3fd3ac9e" | cut -c 10-
     signed_rpms = set()
     output = subprocess.check_output(['koji', 'list-tagged', tag, '--sigs'])
     for line in output.strip().splitlines():
-        key, rpm = line.split(' ')
+        try:
+            key, rpm = line.split(' ')
+        except:
+            # couldn't unpack values... no signature.
+            continue
         if key == KEY_ID:
             signed_rpms.add(rpm)
 
@@ -129,15 +154,26 @@ def main():
     parser = argparse.ArgumentParser(description='Detect package changes in koji and update repository')
     parser.add_argument('dest_dir', help='root directory of the destination repository')
     parser.add_argument('data_dir', help='directory where the script will write or read data from')
+    parser.add_argument('--modify-stable-base', action='store_true',
+                        help='allow modifying the base repository of a stable release')
     args = parser.parse_args()
     dest_dir = args.dest_dir
     data_dir = args.data_dir
 
     for version in VERSIONS:
         for tag in TAGS:
-            print("\n*** %s" % tag)
             if version_from_tag(tag) != version:
                 continue
+
+            print("\n*** %s" % tag)
+
+            if tag in RELEASE_TAGS and version not in DEV_VERSIONS:
+                if args.modify_stable_base:
+                    print("Modification of base repository for stable release %s " % version
+                          + "allowed through the --modify-stable-base switch.")
+                else:
+                    print("Not modifying base repository for stable release %s..." % version)
+                    continue
 
             needs_update = False
 
@@ -161,9 +197,11 @@ def main():
                 sign_unsigned_rpms(tag)
 
                 # export the RPMs from koji
-                subprocess.check_call(['koji', 'dist-repo', tag, '3fd3ac9e',  '--with-src', '--noinherit'])
+                print ("\n-- Make koji write the repository for tag %s" % tag)
+                with_non_latest = [] if tag in RELEASE_TAGS else ['--non-latest']
+                subprocess.check_call(['koji', 'dist-repo', tag, '3fd3ac9e',  '--with-src', '--noinherit'] + with_non_latest)
 
-                # write repo in work directory (we'll sync everything at the end)
+                # write repository to dest_dir
                 write_repo(tag, dest_dir)
 
                 # update data
