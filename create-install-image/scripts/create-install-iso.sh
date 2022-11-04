dc38fe333ab3986376e7e1d0477a6c49fbdf065b
Yann Dirson <yann.dirson@vates.fr>
2022-11-04 19:20:32 +0100

    New script to create install ISO
    Signed-off-by: Yann Dirson <yann.dirson@vates.fr>

---
+++ create-install-image/scripts/create-install-iso.sh
@@ -0,0 +1,260 @@
+#! /bin/bash
+set -e
+set -o pipefail
+
+mydir=$(dirname $0)
+topdir=$mydir/..
+
+. "$mydir/lib/misc.sh"
+
+[ "$(id -u)" != 0 ] || die "should not run as root"
+
+usage() {
+    cat <<EOF
+Usage: $0 [<options>] <dist> <install.img> <output-iso>
+
+Options:
+    -V <VOLID>                (mandatory) ISO volume ID
+    --srcurl <URL>            get RPMs from base-config and overlays from <URL>
+                              default: https://updates.xcp-ng.org/<MAJOR>/<DIST>
+    --netinstall              do not include repository in ISO
+    --sign <NICK> <KEYID>     sign repomd with default gpg key <KEYID>, with readable <NICK>
+    --verbose		      be talkative
+EOF
+}
+
+VERBOSE=
+DOREPO=1
+KEYID=
+KEYNICK=
+SRCURL=
+RPMARCH="x86_64"
+while [ $# -ge 1 ]; do
+    case "$1" in
+        --help|-h)
+            usage
+            exit 0
+            ;;
+        --verbose|-v)
+            VERBOSE=-v
+            ;;
+        -V)
+            [ $# -ge 2 ] || die_usage "$1 needs an argument"
+            VOLID="$2"
+            shift
+            ;;
+        --netinstall)
+            DOREPO=0
+            ;;
+        --srcurl)
+            [ $# -ge 2 ] || die_usage "$1 needs an argument"
+            SRCURL="$2"
+            shift
+            ;;
+        --sign)
+            [ $# -ge 3 ] || die_usage "$1 needs 2 arguments"
+            KEYNICK="$2"
+            KEYID="$3"
+            shift 2
+            ;;
+        -*)
+            die_usage "unknown flag '$1'"
+            ;;
+        *)
+            break
+            ;;
+    esac
+    shift
+done
+
+[ $# = 3 ] || die_usage "need exactly 3 non-option arguments"
+[ -n "$VOLID" ] || die_usage "volume ID must be specified (-V)"
+if [ $DOREPO = 0 -a -n "$KEYID" ]; then
+    die_usage "signing key is useless on netinstall media"
+fi
+
+[ -z "$VERBOSE" ] || set -x
+
+DIST="$1"
+INSTALLIMG="$2"
+OUTISO="$3"
+
+maybe_set_srcurl "$DIST"
+test -r "$INSTALLIMG" || die "cannot read '$INSTALLIMG' for install.img"
+
+command -v genisoimage >/dev/null || die "required tool not found: genisoimage"
+command -v isohybrid >/dev/null || die "required tool not found: isohybrid (syslinux)"
+command -v createrepo_c >/dev/null || die "required tool not found: createrepo_c"
+[ -z "$KEYID" ] || command -v gpg1 >/dev/null || die "required tool not found: gpg1 (gnupg1)"
+
+MKIMAGE=$(command -v grub2-mkimage || command -v grub-mkimage) || die "could not find grub[2]-mkimage"
+if [[ "$($MKIMAGE --version)" =~ ".*2.02" ]]; then
+    die "$MKIMAGE is too old, make sure to have 2.06 installed (XCP-ng package grub-tools)"
+fi
+
+if command -v faketime >/dev/null; then
+    FAKETIME=(faketime "2000-01-01 00:00:00")
+else
+    echo 2>&1 "WARNING: tool not found, disabling support: faketime (libfaketime)"
+    FAKETIME=()
+fi
+
+ISODIR=$(mktemp -d "$TMPDIR/installiso.XXXXXX")
+
+# temporary for storing downloaded files etc
+SCRATCHDIR=$(mktemp -d "$TMPDIR/tmp.XXXXXX")
+
+setup_yum_download "$DIST" "$RPMARCH" "$SRCURL"
+
+
+## put all bits together
+
+test -d "$topdir/templates/iso/$DIST" || die "cannot find dir '$topdir/templates/iso/$DIST'"
+
+# bootloader config files etc. - like "cp -r *", not forgetting .treeinfo
+tar -C "$topdir/templates/iso/$DIST" -cf - . | tar -C "$ISODIR/" -xf - ${VERBOSE}
+
+# initrd
+cp ${VERBOSE} -a "$INSTALLIMG" $ISODIR/install.img
+
+# kernel from rpm
+get_rpms "$SCRATCHDIR" kernel
+rpm2cpio $SCRATCHDIR/kernel-*.rpm | (cd $ISODIR && cpio ${VERBOSE} -idm "*vmlinuz*")
+rm ${VERBOSE} $ISODIR/boot/vmlinuz-*-xen
+mv ${VERBOSE} $ISODIR/boot/vmlinuz-* $ISODIR/boot/vmlinuz
+
+# alt kernel from rpm
+get_rpms "$SCRATCHDIR" kernel-alt
+rpm2cpio $SCRATCHDIR/kernel-alt-*.rpm | (cd $ISODIR && cpio ${VERBOSE} -idm "*vmlinuz*")
+rm ${VERBOSE} $ISODIR/boot/vmlinuz-*-xen
+mkdir ${VERBOSE} $ISODIR/boot/alt
+mv ${VERBOSE} $ISODIR/boot/vmlinuz-* $ISODIR/boot/alt/vmlinuz
+
+# xen from rpm
+get_rpms "$SCRATCHDIR" xen-hypervisor
+rpm2cpio $SCRATCHDIR/xen-hypervisor-*.rpm | (cd $ISODIR && cpio ${VERBOSE} -idm "*xen*gz")
+mv ${VERBOSE} $ISODIR/boot/xen-*-d.gz $ISODIR/boot/xen.gz
+rm ${VERBOSE} $ISODIR/boot/xen-*.gz
+
+
+# Memtest86
+get_rpms "$SCRATCHDIR" memtest86+
+rpm2cpio $SCRATCHDIR/memtest86+-*.rpm | (cd $ISODIR && cpio ${VERBOSE} -idm "./boot/*")
+if [ ! -r $ISODIR/boot/memtest.bin ]; then
+    # older 5.x packaging
+    mv ${VERBOSE} $ISODIR/boot/memtest86+-* $ISODIR/boot/memtest.bin
+fi
+
+
+# optional local repo
+
+sed -i "s,@@TIMESTAMP@@,$(date +%s.00)," \
+    $ISODIR/.treeinfo
+
+if [ $DOREPO = 1 ]; then
+    mkdir ${VERBOSE} "$ISODIR/Packages"
+
+    get_rpms --depends "$ISODIR/Packages" xcp-ng-deps kernel-alt
+
+    createrepo_c ${VERBOSE} "$ISODIR"
+    if [ -n "$KEYID" ]; then
+        gpg1 ${VERBOSE} --default-key="$KEYID" --armor --detach-sign "$ISODIR/repodata/repomd.xml"
+	gpg1 ${VERBOSE} --armor -o "$ISODIR/RPM-GPG-KEY-$KEYNICK" --export "$KEYID"
+	[ -z "$VERBOSE" ] || echo "using key RPM-GPG-KEY-$KEYNICK in .treeinfo"
+	sed -i "s,key1 = .*,key1 = RPM-GPG-KEY-$KEYNICK," \
+	    $ISODIR/.treeinfo
+    else
+	# installer checks if keys are here even when verification is disabled
+	[ -z "$VERBOSE" ] || echo "disabling keys in .treeinfo"
+	sed -i "s,^key,#key," \
+	    $ISODIR/.treeinfo
+
+	# don't try to validate repo sig if we put none
+	[ -z "$VERBOSE" ] || echo "adding no-repo-gpgcheck to boot/isolinux/isolinux.cfg boot/grub/grub*.cfg"
+	sed -i "s,/vmlinuz,/vmlinuz no-repo-gpgcheck," \
+	    $ISODIR/boot/isolinux/isolinux.cfg \
+	    $ISODIR/boot/grub/grub*.cfg
+    fi
+else
+    # no repo
+    # FIXME: should be generated above instead?
+    rm ${VERBOSE} "$ISODIR/.treeinfo"
+
+    # FIXME: trigger netinstall mode?
+fi
+
+
+# BIOS bootloader: isolinux from rpm
+
+get_rpms "$SCRATCHDIR" syslinux
+mkdir "$SCRATCHDIR/syslinux"
+rpm2cpio $SCRATCHDIR/syslinux-*.rpm | (cd "$SCRATCHDIR/syslinux" && cpio ${VERBOSE} -idm "./usr/share/syslinux/*")
+
+cp ${VERBOSE} -p \
+   "$SCRATCHDIR/syslinux/usr/share/syslinux/isolinux.bin" \
+   "$SCRATCHDIR/syslinux/usr/share/syslinux/mboot.c32" \
+   "$SCRATCHDIR/syslinux/usr/share/syslinux/menu.c32" \
+   \
+   $ISODIR/boot/isolinux/
+
+
+## create final ISO
+
+if false; then
+    # grub-mkrescue is "the reference", providing largest platform
+    # support for booting, but OTOH adds tons of stuff we don't need
+    # at all. It was used as a reference to implement UEFI boot, and
+    # is kept handy for when we need it, since the command options are
+    # not that obvious. Eg. we may want to add support for x86 Macs
+    # some day.
+
+    MKRESCUE=$(command -v grub2-mkrescue || command -v grub-mkrescue) || die "could not find grub[2]-mkrescue"
+    # grub2-mkrescue (centos) vs. grub-mkrescue (debian, RoW?)
+    #strace -f -o /tmp/log -s 4096
+    "$MKRESCUE" \
+	--locales= \
+	--modules= \
+	--product-name="XCP-ng" --product-version="$DIST" \
+	\
+	-v \
+	-follow-links \
+	-r -J --joliet-long -V "$VOLID" -input-charset utf-8 \
+	-c boot/isolinux/boot.cat -b boot/isolinux/isolinux.bin \
+	-no-emul-boot -boot-load-size 4 -boot-info-table \
+    \
+	-o "$OUTISO" $ISODIR
+
+else
+    # UEFI bootloader
+
+    BOOTX64=$(mktemp "$TMPDIR/bootx64-XXXXXX.efi")
+
+    # unpack grub-efi.rpm
+    get_rpms "$SCRATCHDIR" grub-efi
+    mkdir "$SCRATCHDIR/grub"
+    rpm2cpio $SCRATCHDIR/grub-efi-*.rpm | (cd "$SCRATCHDIR/grub" && cpio ${VERBOSE} -idm)
+
+    "$MKIMAGE" --directory "$SCRATCHDIR/grub/usr/lib/grub/x86_64-efi" --prefix '()/boot/grub' \
+	       $VERBOSE \
+	       --output "$BOOTX64" \
+	       --format 'x86_64-efi' --compression 'auto' \
+	       'part_gpt' 'part_msdos' 'part_apple' 'iso9660'
+    "${FAKETIME[@]}" mformat -i "$ISODIR/boot/efiboot.img" -N 0 -C -f 2880 -L 16 ::.
+    "${FAKETIME[@]}" mmd     -i "$ISODIR/boot/efiboot.img" ::/efi ::/efi/boot
+    "${FAKETIME[@]}" mcopy   -i "$ISODIR/boot/efiboot.img" "$BOOTX64" ::/efi/boot/bootx64.efi
+
+    # grub modules
+    # FIXME: too many modules?
+    tar -C "$SCRATCHDIR/grub/usr/lib" -cf - grub/x86_64-efi | tar -C "$ISODIR/boot" -xf - ${VERBOSE}
+
+    genisoimage \
+        -o "$OUTISO" \
+	${VERBOSE:- -quiet} \
+        -r -J --joliet-long -V "$VOLID" -input-charset utf-8 \
+        -c boot/isolinux/boot.cat -b boot/isolinux/isolinux.bin \
+        -no-emul-boot -boot-load-size 4 -boot-info-table \
+        -no-emul-boot \
+	-eltorito-alt-boot --efi-boot boot/efiboot.img \
+        $ISODIR
+    isohybrid ${VERBOSE} --uefi "$OUTISO"
+fi
--- create-install-image/scripts/lib/misc.sh
+++ create-install-image/scripts/lib/misc.sh
@@ -51,3 +51,77 @@ trap 'exitcleanup' EXIT INT
 # See https://unix.stackexchange.com/questions/92257/
 export TMPDIR=$(mktemp -d "$PWD/tmpdir-XXXXXX")
 CLEANUP_DIRS+=("$TMPDIR")
+
+
+# infrastructure for fetching RPMs from source repo
+
+yumdl_is_dnf() {
+    yumdownloader --version | grep -q dnf
+}
+
+setup_yum_download() {
+    [ $# = 3 ] || die "setup_yum_download: need exactly 3 arguments"
+    DIST="$1"
+    RPMARCH="$2"
+    SRCURL="$3"
+
+    YUMDLCONF=$(mktemp "$TMPDIR/yum-XXXXXX.conf")
+    YUMLOGDIR=$(mktemp -d "$TMPDIR/logs-XXXXXX")
+    DUMMYROOT=$(mktemp -d "$TMPDIR/root-XXXXXX")
+
+    if yumdl_is_dnf; then
+        enable_plugins=1
+        echo >&2 "WARNING: yumdownloader is dnf wrapper, I have to enable dnf plugins!"
+    else
+        enable_plugins=0
+    fi
+
+    # FIXME gpgcheck
+    cat > "$YUMDLCONF" <<EOF
+[main]
+reposdir=/dev/null
+cachedir=$TMPDIR/yum-cache
+persistdir=/dev/null
+gpgcheck=0
+repo_gpgcheck=0
+plugins=${enable_plugins}
+basearch=$RPMARCH
+[xcpng-base]
+name=xcpng-base
+baseurl=$SRCURL/base/$RPMARCH/
+[xcpng-updates]
+name=xcpng-updates
+baseurl=$SRCURL/updates/$RPMARCH/
+[xcpng-testing]
+name=xcpng-testing
+baseurl=$SRCURL/testing/$RPMARCH/
+EOF
+    mkdir ${VERBOSE} "$DUMMYROOT/etc"
+    YUMDLFLAGS=(
+        # non-$VERBOSE is -q, $VERBOSE is default, yum's -v would be debug
+        $([ -n "$VERBOSE" ] || printf -- "-q")
+        --config="$YUMDLCONF"
+        --releasever="$DIST"
+        # --disablerepo="*" --enablerepo='xcpng-base'
+    )
+
+    # availability of yumdownloader does not imply that of yum
+    local YUM=$(command -v yum || command -v dnf) || die "no yum or dnf found"
+    # summary of repos
+    "$YUM" ${YUMDLFLAGS[@]} repolist all
+}
+
+get_rpms() {
+    local OPTS=""
+    if [ "$1" = "--depends" ]; then
+        OPTS="--resolve"
+        shift
+    fi
+    local DESTDIR="$1"
+    shift
+    if [ -n "$YUMDLFLAGS" ]; then
+        (cd "$DESTDIR" && yumdownloader $OPTS "${YUMDLFLAGS[@]}" --installroot="$DUMMYROOT" "$@")
+    else
+        die "Must configure yum download before attempting to download"
+    fi
+}
