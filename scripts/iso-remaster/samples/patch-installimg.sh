ca19a63cfc9c86d18a49bac138935990f1f0dddb
Yann Dirson <yann.dirson@vates.fr>
2023-06-02 17:21:16 +0200

    iso-remaster: improve patch-installimg sample
    More comments, and an example for adding extra RPMs.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.fr>

--- scripts/iso-remaster/samples/patch-installimg.sh
+++ scripts/iso-remaster/samples/patch-installimg.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 set -e
 
+# unpacked rootfs to modify
 INSTALLIMG="$1"
 
-## copies a few files too much, but that's harmless
+## Include a locally-modified version of the installer for testing
+## - copies a few files too much until we can "make install", but that's harmless
+#
 #HOSTINSTALLER=$HOME/src/host-installer
 #cp -rv "$HOSTINSTALLER"/* "$INSTALLIMG/opt/xensource/installer/"
+
+## Install extra packages - use `rpm` not `yum`, as we had to use `rpm
+## --nodeps` during image creation and `yum` will now go on strike.
+## Luckily `yumdownloader` still works.
+#
+#DLDIR="$(mktemp -d)"
+#trap "rm -r '$DLDIR'" EXIT INT
+#
+#yumdownloader --installroot="$INSTALLIMG" --destdir="$DLDIR" --resolve --enablerepo=epel ndisc6 -y
+#fakechroot rpm --root="$INSTALLIMG" --install "$DLDIR/*.rpm"
