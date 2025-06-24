737f254a13f33d0c0e243989a103f320c91fe44d
Yann Dirson <yann.dirson@vates.tech>
2025-06-24 12:29:19 +0200

    iso-remaster: cleanup samples
    Hopefully makes sample snippets easier to use.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- scripts/iso-remaster/samples/patch-iso.sh
+++ scripts/iso-remaster/samples/patch-iso.sh
@@ -3,62 +3,34 @@ set -e
 
 ISODIR="$1"
 
-## remove existing repo sig
-#rm -fv "$ISODIR/repodata/repomd.xml.asc"
-
-## remove old repo metadata
-#rm -r "$ISODIR/repodata/"
-
-
-## example: customize theme
-## remove old rpms
-#for p in $(cat <<EOF
-#                xcp-ng-plymouth-theme-1.0.0-7.xcpng8.3.noarch.rpm
-#                xcp-ng-release-8.3.0-2.x86_64.rpm
-#                xcp-ng-release-config-8.3.0-2.x86_64.rpm
-#                xcp-ng-release-presets-8.3.0-2.x86_64.rpm
-#                xsconsole-10.1.13-1.xcpng8.3.x86_64.rpm
-#EOF
-#          )
-#do
-#    rm "$ISODIR/Packages/$p"
-#done
-#
-## add the new ones
-#for p in $(cat <<EOF
-#                xcp-ng-plymouth-theme-1.0.0-7.xcpng8.3+newtheme1.noarch.rpm
-#                xcp-ng-release-8.3.0-2+newtheme1.x86_64.rpm
-#                xcp-ng-release-config-8.3.0-2+newtheme1.x86_64.rpm
-#                xcp-ng-release-presets-8.3.0-2+newtheme1.x86_64.rpm
-#                xsconsole-10.1.13-1.xcpng8.3+newtheme2.x86_64.rpm
-#EOF
-#          )
-#do
-#    cp "$HOME/newtheme/$p" "$ISODIR/Packages/"
-#done
-#
-## installer splash
-#cp "$HOME/src/xcp/iso/8.3/boot/isolinux/splash.lss" "$ISODIR/boot/isolinux/"
-
-
-## regenerate repodata
-#createrepo_c "$ISODIR"
-
-## patches to kernel commandline
+# patches to kernel commandline
 SED_COMMANDS=()
 # harmless no-op substitution in case no other is added
 SED_COMMANDS+=(-e "s@^@@")
 
-# add `no-repo-gpgcheck` so a modified repo will be accepted
-#SED_COMMANDS+=(-e "s@/vmlinuz@/vmlinuz no-repo-gpgcheck@")
-#SED_COMMANDS+=(-e "s@/vmlinuz@/vmlinuz network_device=lacp:members=eth0,eth1@")
-#SED_COMMANDS+=(-e "s@/vmlinuz@/vmlinuz install answerfile=http://pxe/configs/custom/ydi/lacp.xml@")
-#SED_COMMANDS+=(-e "s@/vmlinuz@/vmlinuz atexit=shell@")
+# # update some RPMs:
+# # * remove old repo metadata
+# rm -r "$ISODIR/repodata/"
+# # * replace RPMs
+# cp -p ~/my/Packages/*.rpm "$ISODIR/Packages/"
+# # * regenerate repo metadata
+# createrepo_c "$ISODIR"
+# # * add `no-repo-gpgcheck` so a modified repo will be accepted
+# SED_COMMANDS+=(-e "s@/vmlinuz@/vmlinuz no-repo-gpgcheck@")
+
+# # prevent any reboot on installer error to allow investigating
+# SED_COMMANDS+=(-e "s@/vmlinuz@/vmlinuz atexit=shell@")
+
+# # activate ssh to installer with given password, eg. to collect logs
+# SED_COMMANDS+=(-e "s@/vmlinuz@/vmlinuz network_device=all sshpassword=passw0rd@")
+
+# # get an answerfile over the network
+# SED_COMMANDS+=(-e "s@/vmlinuz@/vmlinuz install answerfile=http://pxe/configs/preset.xml@")
 
 sed -i "${SED_COMMANDS[@]}" \
     "$ISODIR"/*/*/grub*.cfg \
     "$ISODIR"/boot/isolinux/isolinux.cfg
 
-## sign with a different key
-#gpg1 --armor --detach-sign "$ISODIR/repodata/repomd.xml"
-#gpg1 --armor --export > "$ISODIR/RPM-GPG-KEY-xcpng"
+# # sign with a different key (needs more work)
+# gpg1 --armor --detach-sign "$ISODIR/repodata/repomd.xml"
+# gpg1 --armor --export > "$ISODIR/RPM-GPG-KEY-xcpng"
