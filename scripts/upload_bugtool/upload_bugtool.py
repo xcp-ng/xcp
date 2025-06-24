d4b91c7222554f38cadb526865f7882781aefb55
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-06-24 09:46:35 +0200

    remove version_lt version comparer
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- scripts/upload_bugtool/upload_bugtool.py
+++ scripts/upload_bugtool/upload_bugtool.py
@@ -30,15 +30,6 @@ except ImportError:
     PLATFORM_VERSION = "9999"  # failsafe value, assume dev build
 
 
-def version_lt(a, b):
-    for ai, bi in zip_longest(a, b, fillvalue=0):
-        if ai < bi:
-            return True
-        elif ai > bi:
-            return False
-    return False
-
-
 def parse_share_link(share_link):
     """
     Parses the Nextcloud/Owncloud share link to extract the base URL and token.
@@ -84,7 +75,7 @@ def upload(base_url, folder_token, password, file):
     ]
     # fmt: on
     platform_version = tuple(int(x) for x in PLATFORM_VERSION.split("."))
-    if version_lt(platform_version, (3, 3, 0)):
+    if platform_version < (3, 3, 0):
         curl_args += [
             "--ciphers",
             "ECDHE-RSA-AES256-SHA384,ECDHE-RSA-AES256-GCM-SHA384,AES256-SHA256,AES128-SHA256,ECDHE-ECDSA-AES128-GCM-SHA256",
