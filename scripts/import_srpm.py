3937ec5be4fe92993ac2d1fc8aaeff7b3ad72eb3
Gaëtan Lehmann <gaetan.lehmann@vates.tech>
2025-08-04 16:02:33 +0200

    import_srpm: allow to pass a url as source file
    Signed-off-by: Gaëtan Lehmann <gaetan.lehmann@vates.tech>

--- scripts/import_srpm.py
+++ scripts/import_srpm.py
@@ -6,7 +6,11 @@ import logging
 import os
 import shutil
 import subprocess
+from contextlib import nullcontext
 from glob import glob
+from tempfile import TemporaryDirectory
+from urllib.parse import urlparse
+from urllib.request import urlretrieve
 
 
 def call_process(args):
@@ -40,6 +44,13 @@ def pipe_commands(*commands: list[str]) -> bytes:
 
     return final_stdout
 
+def is_url(url_string):
+    try:
+        result = urlparse(url_string)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
 def main():
     parser = argparse.ArgumentParser(description='Imports the contents of a source RPM into a git repository')
     parser.add_argument('source_rpm', help='local path to source RPM')
@@ -64,110 +75,121 @@ def main():
         if shutil.which(dep) is None:
             parser.error(f"{dep} can't be found.")
 
-    # check that the source RPM file exists
-    if not os.path.isfile(args.source_rpm):
-        parser.error("File %s does not exist." % args.source_rpm)
-    if not args.source_rpm.endswith('.src.rpm'):
-        parser.error("File %s does not appear to be a source RPM." % args.source_rpm)
-    source_rpm_abs = os.path.abspath(args.source_rpm)
+    # handle URL source
+    source_rpm = args.source_rpm
+    is_remote_rpm = is_url(source_rpm)
 
-    # enter repository directory
-    if not os.path.isdir(args.repository):
-        parser.error("Repository directory %s does not exist." % args.repository)
-    os.chdir(args.repository)
+    with TemporaryDirectory() if is_remote_rpm else nullcontext() as temp_dir:
+        if is_remote_rpm:
+            # get the src.rpm locally, and continue with the actual file
+            local_filename = f'{temp_dir}/{os.path.basename(source_rpm)}'
+            urlretrieve(source_rpm, local_filename)
+            source_rpm = local_filename
 
-    # check that the working copy is clean
-    try:
-        call_process(['git', 'diff-index', '--quiet',  'HEAD', '--'])
-        print("Working copy is clean.")
-    except:
-        raise
-        parser.error("Git repository seems to have local modifications.")
+        # check that the source RPM file exists
+        if not os.path.isfile(source_rpm):
+            parser.error("File %s does not exist." % source_rpm)
+        if not source_rpm.endswith('.src.rpm'):
+            parser.error("File %s does not appear to be a source RPM." % source_rpm)
+        source_rpm_abs = os.path.abspath(source_rpm)
 
-    # check that there are no untracked files
-    if len(subprocess.check_output(['git', 'ls-files', '--others', '--exclude-standard'])):
-        parser.error("There are untracked files.")
+        # enter repository directory
+        if not os.path.isdir(args.repository):
+            parser.error("Repository directory %s does not exist." % args.repository)
+        os.chdir(args.repository)
 
-    print(" checking out parent ref...")
+        # check that the working copy is clean
+        try:
+            call_process(['git', 'diff-index', '--quiet', 'HEAD', '--'])
+            print("Working copy is clean.")
+        except:
+            raise
+            parser.error("Git repository seems to have local modifications.")
 
-    if args.push:
-        call_process(['git', 'fetch'])
-    call_process(['git', 'checkout', args.parent_branch])
-    if args.push:
-        call_process(['git', 'pull'])
+        # check that there are no untracked files
+        if len(subprocess.check_output(['git', 'ls-files', '--others', '--exclude-standard'])):
+            parser.error("There are untracked files.")
 
-    print(" removing everything from SOURCES and SPECS...")
+        print(" checking out parent ref...")
 
-    if os.path.isdir('SOURCES') and len(os.listdir('SOURCES')) > 0:
-        call_process(['git', 'rm', 'SOURCES/*', '-r'])
-    if os.path.isdir('SOURCES') and len(os.listdir('SOURCES')) > 0:
-        parser.error("Files remaining in SOURCES/ after removing the tracked ones. ")
-        parser.error("Delete them (including hidden files), reset --hard.")
-    os.mkdir('SOURCES')
+        if args.push:
+            call_process(['git', 'fetch'])
+        call_process(['git', 'checkout', args.parent_branch])
+        if args.push:
+            call_process(['git', 'pull'])
 
-    if os.path.isdir('SPECS'):
-        call_process(['git', 'rm', 'SPECS/*', '-r'])
-    os.mkdir('SPECS')
+        print(" removing everything from SOURCES and SPECS...")
 
-    print(" extracting SRPM...")
+        if os.path.isdir('SOURCES') and len(os.listdir('SOURCES')) > 0:
+            call_process(['git', 'rm', 'SOURCES/*', '-r'])
+        if os.path.isdir('SOURCES') and len(os.listdir('SOURCES')) > 0:
+            parser.error("Files remaining in SOURCES/ after removing the tracked ones. ")
+            parser.error("Delete them (including hidden files), reset --hard.")
+        os.mkdir('SOURCES')
 
-    os.chdir('SOURCES')
-    pipe_commands(['rpm2cpio', source_rpm_abs], ['cpio', '-idmv'])
-    os.chdir('..')
-    for f in glob('SOURCES/*.spec'):
-        shutil.move(f, 'SPECS')
+        if os.path.isdir('SPECS'):
+            call_process(['git', 'rm', 'SPECS/*', '-r'])
+        os.mkdir('SPECS')
 
-    print(" removing trademarked or copyrighted files...")
+        print(" extracting SRPM...")
 
-    sources = os.listdir('SOURCES')
-    deletemsg = "File deleted from the original sources for trademark-related or copyright-related legal reasons.\n"
-    deleted = []
-    for f in ['Citrix_Logo_Black.png', 'COPYING.CitrixCommercial']:
-        if f in sources:
-            os.unlink(os.path.join('SOURCES', f))
-            open(os.path.join('SOURCES', "%s.deleted-by-XCP-ng.txt" % f), 'w').write(deletemsg)
-            deleted.append(f)
+        os.chdir('SOURCES')
+        pipe_commands(['rpm2cpio', source_rpm_abs], ['cpio', '-idmv'])
+        os.chdir('..')
+        for f in glob('SOURCES/*.spec'):
+            shutil.move(f, 'SPECS')
 
-    if subprocess.call(['git', 'rev-parse', '--quiet', '--verify', args.branch]) != 0:
-        call_process(['git', 'checkout', '-b', args.branch])
-    else:
-        call_process(['git', 'checkout', args.branch])
-    call_process(['git', 'add', '--all'])
+        print(" removing trademarked or copyrighted files...")
 
-    print(" committing...")
-    has_changes = False
-    try:
-        call_process(['git', 'diff-index', '--quiet',  'HEAD', '--'])
-    except:
-        has_changes = True
+        sources = os.listdir('SOURCES')
+        deletemsg = "File deleted from the original sources for trademark-related or copyright-related legal reasons.\n"
+        deleted = []
+        for f in ['Citrix_Logo_Black.png', 'COPYING.CitrixCommercial']:
+            if f in sources:
+                os.unlink(os.path.join('SOURCES', f))
+                open(os.path.join('SOURCES', "%s.deleted-by-XCP-ng.txt" % f), 'w').write(deletemsg)
+                deleted.append(f)
 
-    if not has_changes:
-        print("\nWorking copy has no modifications. Nothing to commit. No changes from previous release?\n")
-    else:
-        msg = 'Import %s' % os.path.basename(args.source_rpm)
-        if deleted:
-            msg += "\n\nFiles deleted for legal reasons:\n - " + '\n - '.join(deleted)
-        call_process(['git', 'commit', '-s', '-m', msg])
+        if subprocess.call(['git', 'rev-parse', '--quiet', '--verify', args.branch]) != 0:
+            call_process(['git', 'checkout', '-b', args.branch])
+        else:
+            call_process(['git', 'checkout', args.branch])
+        call_process(['git', 'add', '--all'])
 
-    # tag
-    if args.tag is not None:
-        call_process(['git', 'tag', args.tag])
+        print(" committing...")
+        has_changes = False
+        try:
+            call_process(['git', 'diff-index', '--quiet',  'HEAD', '--'])
+        except:
+            has_changes = True
 
-    # push to remote
-    if args.push:
-        call_process(['git', 'push', '--set-upstream', 'origin', args.branch])
+        if not has_changes:
+            print("\nWorking copy has no modifications. Nothing to commit. No changes from previous release?\n")
+        else:
+            msg = 'Import %s' % os.path.basename(source_rpm)
+            if deleted:
+                msg += "\n\nFiles deleted for legal reasons:\n - " + '\n - '.join(deleted)
+            call_process(['git', 'commit', '-s', '-m', msg])
+
+        # tag
         if args.tag is not None:
-            call_process(['git', 'push', 'origin', args.tag])
+            call_process(['git', 'tag', args.tag])
 
-    print(" switching to master before leaving...")
+        # push to remote
+        if args.push:
+            call_process(['git', 'push', '--set-upstream', 'origin', args.branch])
+            if args.tag is not None:
+                call_process(['git', 'push', 'origin', args.tag])
 
-    call_process(['git', 'checkout', 'master'])
+        print(" switching to master before leaving...")
 
-    # merge to master if needed
-    if args.push and args.master:
-        print(" merging to master...")
-        call_process(['git', 'push', 'origin', '%s:master' % args.branch])
-        call_process(['git', 'pull'])
+        call_process(['git', 'checkout', 'master'])
+
+        # merge to master if needed
+        if args.push and args.master:
+            print(" merging to master...")
+            call_process(['git', 'push', 'origin', '%s:master' % args.branch])
+            call_process(['git', 'pull'])
 
 
 if __name__ == "__main__":
