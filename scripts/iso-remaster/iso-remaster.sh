68581e3d394000db63087696d478c6e0a4986d89
Yann Dirson <yann.dirson@vates.tech>
2025-12-17 14:38:56 +0100

    iso-remaster: properly implement -z
    Original implementation took care of the flag, but did not apply the
    selected compression algorithm.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- scripts/iso-remaster/iso-remaster.sh
+++ scripts/iso-remaster/iso-remaster.sh
@@ -192,14 +192,14 @@ esac
 # maybe run install.img patcher
 
 if [ -n "$IMGPATCHER" ]; then
-    bzcat "$SRCISO/install.img" | (cd "$INSTALLIMG" && "${FAKEROOT[@]}" cpio -idm)
+    $ZCAT "$SRCISO/install.img" | (cd "$INSTALLIMG" && "${FAKEROOT[@]}" cpio -idm)
 
     # patch install.img contents
     "${FAKEROOT[@]}" "$IMGPATCHER" "$INSTALLIMG" || die "IMG patcher exited in error: $?"
 
     # repack install.img
     (cd "$INSTALLIMG" && "${FAKEROOT[@]}" sh -c "find . | cpio -o -H newc") |
-        bzip2 > "$DESTISO/install.img"
+        $ZIP > "$DESTISO/install.img"
 fi
 
 # produce merged view
