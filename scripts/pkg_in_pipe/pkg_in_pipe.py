22e1dec1746bc555e516c2c259dbc071ed1b9cd6
Vincent Michel <vincent.michel@vates.tech>
2026-09-18 12:44:04 +0200

    [pkg_in_pipe] Add a missing sources toml dictionary for koji builds
    It might happen that the previous tag build for a package does not have
    its source field filled in with the corresponding git commit (this
    typically happens for security updates that did not go through the
    standard release procedure). In order to avoid reporting extra PRs and
    plane cards, those missing sources can now be listed in the
    `missing_sources.toml` dictionary.
    
    Signed-off-by: Vincent Michel <vincent.michel@vates.tech>

--- scripts/pkg_in_pipe/pkg_in_pipe.py
+++ scripts/pkg_in_pipe/pkg_in_pipe.py
@@ -4,6 +4,7 @@ import io
 import json
 import os
 import re
+import tomllib
 from collections import defaultdict
 from datetime import datetime
 from textwrap import dedent
@@ -17,6 +18,10 @@ import requests
 from github.Commit import Commit
 from github.GithubException import BadCredentialsException
 from github.PullRequest import PullRequest
+
+
+with open(os.path.join(os.path.dirname(__file__), 'missing_sources.toml'), 'rb') as missing_sources_file:
+    MISSING_SOURCES = tomllib.load(missing_sources_file)
 
 def print_header(out):
     print(dedent('''
@@ -219,7 +224,7 @@ def find_previous_build_commit(build_tag, build):
 
     for tag in tagged:
         previous_build = get_koji_build(tag['build_id'])
-        source = previous_build.get('source')
+        source = previous_build.get('source') or MISSING_SOURCES.get(previous_build['nvr'])
         if not source:
             continue
         return parse_source(source)[1]
